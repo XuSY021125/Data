757cc1b52c93237ab4c57a44765dbf42f0cf143d
XuSY <91069819+XuSY021125@users.noreply.github.com>
2022-10-31 23:48:20 +0800

    Add files via upload

---
+++ BiSearch1.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h> 
+int locate(int a[],int left,int right)
+{
+
+	int mid,target;
+	printf("ÇëÊäÈëtargetµÄÖµ£º");
+	scanf("%d",&target); 
+	while(left<=right)
+	{
+		mid=(left+right)/2;
+		if(a[mid]==target)
+		{
+			return mid;
+			
+		}
+		else if(a[mid]<target)
+		{
+			left=mid+1;
+		
+		}
+		else if(a[mid]>target)
+		{
+			right=mid-1;
+			
+		}
+		else if(left==right)
+		{
+			mid=(a[mid]==target)?mid:-1;
+			return mid;
+		}
+
+	 }
+}
+int main()
+{
+	int a[8]={1,4,6,8,10,11,12,16};
+	int mid=locate(a,0,7);
+	printf("%d",mid);
+}
