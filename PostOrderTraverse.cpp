ddb9a44fe5bfa35f66421a2762a0fde848018b32
XuSY <91069819+XuSY021125@users.noreply.github.com>
2023-02-19 23:10:46 +0800

    Update PostOrderTraverse.cpp

--- PostOrderTraverse.cpp
+++ PostOrderTraverse.cpp
@@ -2,12 +2,11 @@
 #include<stdlib.h> 
 #include <string.h>
 #define TElemType int
-//构造结点的结构体
-typedef struct BiTNode{
-    TElemType data;//数据域
-    struct BiTNode *lchild,*rchild;//左右孩子指针
+typedef struct BiTNode
+{
+    TElemType data;
+    struct BiTNode *lchild,*rchild;
 }BiTNode,*BiTree;
-//初始化树的函数
 void CreateBiTree(BiTree *T){
     *T=(BiTNode*)malloc(sizeof(BiTNode));
     (*T)->data=1;
@@ -33,18 +32,16 @@ void CreateBiTree(BiTree *T){
     (*T)->lchild->lchild->lchild=NULL;
     (*T)->lchild->lchild->rchild=NULL;
 }
-//模拟操作结点元素的函数，输出结点本身的数值
 void displayElem(BiTNode* elem){
     printf("%d ",elem->data);
 }
 //后序遍历
 void PostOrderTraverse(BiTree T){
     if (T) {
-        PostOrderTraverse(T->lchild);//遍历左孩子
-        PostOrderTraverse(T->rchild);//遍历右孩子
-        displayElem(T);//调用操作结点数据的函数方法
+        PostOrderTraverse(T->lchild);
+        PostOrderTraverse(T->rchild);
+        displayElem(T);
     }
-    //如果结点为空，返回上一层
     return;
 }
 
