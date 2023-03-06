4b5e5045e5bd63b069b3e1db271c7e7fd45288c3
XuSY <91069819+XuSY021125@users.noreply.github.com>
2023-03-06 18:10:45 +0800

    Add files via upload

--- 367.有效的完全平方数.cpp
+++ 367.有效的完全平方数.cpp
@@ -3,16 +3,19 @@ int main()
 {
     int num;
     printf("ÇëÊäÈënumµÄÖµ;");
-    scanf_s("%d", &num);
+    scanf("%d", &num);
     int min = 1, max = num, mid;
     while (min <= max) {
         mid = (min + min) / 2;
         if (num / mid == mid && num % mid == 0) 
+        {
+        	printf("mid=%d",mid); 
             return true;
-        else if (num / mid < mid)
-            max = mid - 1;
-        else
+    	}
+		else if (mid * mid < num)
             min = mid + 1;
+        else
+            max = mid - 1;
     }
     return false;
 }
